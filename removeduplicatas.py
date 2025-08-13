487d445671a82c849f8cd4c051a785ec55874768
nataliecavalcante <natalie_cs1@hotmail.com>
2025-08-13 13:24:58 -0300

    Solução concluída - removeduplicatas.py

---
+++ removeduplicatas.py
@@ -0,0 +1,11 @@
+def valores_unicos(lista):
+    vistos = set()
+    unicos = []
+    for item in lista:
+        if item not in vistos:
+            vistos.add(item)
+            unicos.append(item)
+    return unicos
+lista = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+resultado = valores_unicos(lista)
+print(resultado)
